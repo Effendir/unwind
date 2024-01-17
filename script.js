19a2390ed447a124e54b28848ae77bf13e44dbe4
eugene <eugene.dami@yahoo.fr>
2024-01-17 13:22:25 +0100

    define a mailing form, front to back

--- script.js
+++ script.js
@@ -22,4 +22,36 @@ function light_switch() {
 
 }
 
+function mail_form () {
+  const cta = document.querySelector(".cta")
+  const mail_button = document.querySelector("#send-mail");
+  const form = '<form id="contact-form"><input type="hidden" name="contact_number"><label>Name</label><br><input type="text" name="user_name"><br><label>Email</label><br><input type="email" name="user_email"><br><label>Message</label><br><textarea name="message"></textarea><br><input type="submit" value="Send"><br></form>';
+
+  mail_button.addEventListener("click", () => {
+    cta.innerHTML = form;
+    send_mail();
+  });
+
+}
+
+function send_mail() {
+  (function() {
+    emailjs.init('KaeIisZjDCiWkL-yt');
+  })();
+
+  window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+  }
+}
+
 light_switch();
+mail_form();
