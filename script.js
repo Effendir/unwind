94d7ff3c298f0f277e9ca03cae6dc9745046a48f
eugene dami <eugene.dami@yahoo.fr>
2024-01-30 12:50:44 +0100

    fix mailing form and some styling

--- script.js
+++ script.js
@@ -20,37 +20,40 @@ function toggleLightSwitch() {
 }
 
 function displayMailForm () {
-  const cta = document.querySelector(".cta")
+  const form = document.querySelector("#contact-form")
   const sendMailButton = document.querySelector("#send-mail");
-  const form = '<form id="contact-form"><input type="hidden" name="contact_number"><label>Name</label><input type="text" name="user_name"><label>Email</label><input type="email" name="user_email"><label>Message</label><textarea name="message"></textarea><input type="submit" value="Send" id="send-button"></form>';
 
   sendMailButton.addEventListener("click", () => {
-    cta.innerHTML = form;
-    sendMail();
+    form.style.display = "flex";
+    sendMailButton.style.display = "none"
   });
 
 }
 
 function sendMail() {
   (function() {
-    emailjs.init('KaeIisZjDCiWkL-yt');
+      // https://dashboard.emailjs.com/admin/account
+      emailjs.init('KaeIisZjDCiWkL-yt');
   })();
 
   window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        this.contact_number.value = Math.random() * 100000 | 0;
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-                alert("Your mail has been sent :)");
-            }, function(error) {
-                console.log('FAILED...', error);
-                alert("Something went wrong, sorry for the inconvenience...");
-            });
-    });
+      document.getElementById('contact-form').addEventListener('submit', function(event) {
+          event.preventDefault();
+          // generate a five digit number for the contact_number variable
+          this.contact_number.value = Math.random() * 100000 | 0;
+          // these IDs from the previous steps
+          emailjs.sendForm('contact_service', 'contact_form', this)
+              .then(function() {
+                  console.log('SUCCESS!');
+                  alert("Mail sent :)")
+              }, function(error) {
+                  console.log('FAILED...', error);
+                  alert("Technical issue, sorry for the inconvenience")
+              });
+      });
   }
 }
 
 toggleLightSwitch();
 displayMailForm();
+sendMail();
